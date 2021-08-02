8fbf580b5bdc55308563f2981069510ccabbbc94
siren <etrentson@gmail.com>
2021-08-02 13:18:06 -0400

    BOT-DETECTED

--- server.js
+++ server.js
@@ -1,76 +1,76 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const tmi = require('tmi.js');
+// const tmi = require('tmi.js');
 
-const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+// const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
-const nuuriSip = 'You need to drink some water, Cohh.'
+// const nuuriSip = 'You need to drink some water, Cohh.'
 
-const giftBomb = 'cohhBomb INCOMING GIFT BOMB cohhBomb'
+// const giftBomb = 'cohhBomb INCOMING GIFT BOMB cohhBomb'
 
-const helloBomb = "The Hello Queue and Center's are open!! From this point on, any cohhHi's will be added to the daily hello list or appear in the hicenter! cohhGG"
+// const helloBomb = "The Hello Queue and Center's are open!! From this point on, any cohhHi's will be added to the daily hello list or appear in the hicenter! cohhGG"
 
-const chatLove = "Chat cohhL HAS BEEN CLAIMED! cohhGV Thanks to"
+// const chatLove = "Chat cohhL HAS BEEN CLAIMED! cohhGV Thanks to"
 
-const commands = {
-    nuuriSip: {
-        response: 'Hydrate time! cohhSip cohhSip cohhSip'
-    },
-    duck: {
-        response: 'Hide non-subs!'
-    },
-    chatLove:{
-        response: 'cohhL cohhL cohhL'
-    }
-}
+// const commands = {
+//     nuuriSip: {
+//         response: 'Hydrate time! cohhSip cohhSip cohhSip'
+//     },
+//     duck: {
+//         response: 'Hide non-subs!'
+//     },
+//     chatLove:{
+//         response: 'cohhL cohhL cohhL'
+//     }
+// }
 
-const client = new tmi.Client({
-    connection: { 
-        reconnect: true
-    },
-    channels: [
-        'cohhcarnage'
-    ],
-    identity: {
-        username: process.env.TWITCH_BOT_USERNAME,
-        password: process.env.TWITCH_OAUTH_TOKEN
-    }
-});
+// const client = new tmi.Client({
+//     connection: { 
+//         reconnect: true
+//     },
+//     channels: [
+//         'cohhcarnage'
+//     ],
+//     identity: {
+//         username: process.env.TWITCH_BOT_USERNAME,
+//         password: process.env.TWITCH_OAUTH_TOKEN
+//     }
+// });
 
-client.on('message', onMessageHandler);
-client.on('connected', onConnectHandler);
+// client.on('message', onMessageHandler);
+// client.on('connected', onConnectHandler);
 
-client.connect();
+// client.connect();
 
-function onMessageHandler (target, context, msg, self) {
-    if (self) { return; } //ignore messages from bot
+// function onMessageHandler (target, context, msg, self) {
+//     if (self) { return; } //ignore messages from bot
 
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
+//     // Remove whitespace from chat message
+//     const commandName = msg.trim();
     
-    // If the command is known, let's execute it
-    if (commandName === nuuriSip) {
+//     // If the command is known, let's execute it
+//     if (commandName === nuuriSip) {
         
-        client.say(target, 'cohhSip Hydrate! cohhSip');
-        console.log(`* Executed ${commandName} command`);
-    } else if (commandName === giftBomb) {
+//         client.say(target, 'cohhSip Hydrate! cohhSip');
+//         console.log(`* Executed ${commandName} command`);
+//     } else if (commandName === giftBomb) {
 
-        client.say(target, 'cohhBomb cohhBlind Hide non-subs! cohhBlind cohhBomb');
-        console.log(`* Executed ${commandName} command`);
+//         client.say(target, 'cohhBomb cohhBlind Hide non-subs! cohhBlind cohhBomb');
+//         console.log(`* Executed ${commandName} command`);
         
-    } else if (commandName === helloBomb) {
+//     } else if (commandName === helloBomb) {
 
-        client.say(target, 'cohhGHi cohhHi cohhGV cohhLurk');
-        console.log(`* Executed ${commandName} command`);
+//         client.say(target, 'cohhGHi cohhHi cohhGV cohhLurk');
+//         console.log(`* Executed ${commandName} command`);
 
-    } else if (commandName === chatLove) {
+//     } else if (commandName === chatLove) {
 
-        client.say(target, 'cohhL cohhL cohhL');
-        console.log(`* Executed ${commandName} command`);
+//         client.say(target, 'cohhL cohhL cohhL');
+//         console.log(`* Executed ${commandName} command`);
     
-    }
-}
+//     }
+// }
 
-function onConnectHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-}
+// function onConnectHandler (addr, port) {
+//     console.log(`* Connected to ${addr}:${port}`);
+// }
